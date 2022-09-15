a86b0d467d26b3c4d725c1ad85ea7b26a6052acc
Wanjau88 <patrickkamauwanjau@gmail.com>
2022-09-15 06:13:28 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * main - print putchar
+ * main -  prints Holberton, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('P');
+	_putchar('_');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
