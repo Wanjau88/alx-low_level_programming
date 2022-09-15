e5f4fc352f2cb644309d393b010527bdedc9b05b
Wanjau88 <patrickkamauwanjau@gmail.com>
2022-09-15 13:05:43 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,15 @@
 
 /**
  * _isalpha - Shows 1 if the input is a
- * letter Another cases, shows 0
+ * 0
  *
  * @c: The character in ASCII code
  *
  * Return: 1 for letters. 0 for the rest.
  */
-int _isalpha(int c)
+int _islower(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
@@ -20,4 +20,3 @@ int _isalpha(int c)
 	}
 	_putchar('\n');
 }
-
